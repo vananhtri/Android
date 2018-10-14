4f057f2660865e874c0bd5a82880e42c0a80d9a5
vananhtri <42496677+vananhtri@users.noreply.github.com>
2018-10-14 14:05:23 +0700

    home

--- CodeDoAn/app/src/main/java/com/example/vananh/doan/ChucNang/Home.java
+++ CodeDoAn/app/src/main/java/com/example/vananh/doan/ChucNang/Home.java
@@ -4,9 +4,14 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.vananh.doan.R;
 import com.example.vananh.doan.menuActivity;
@@ -16,10 +21,50 @@ public class Home extends android.support.v4.app.Fragment {
     public Home() {
         // Required empty public constructor
     }
-
+    LinearLayout lnLamBai, lnHuongDan, lnMeo;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        lnLamBai=getActivity().findViewById(R.id.LinearLamBai);
+        lnHuongDan=getActivity().findViewById(R.id.LinearHươngDan);
+        lnMeo=getActivity().findViewById(R.id.LinearMeo);
+        lnLamBai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LamBaiThi fragment = new LamBaiThi();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.content_main, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+        lnHuongDan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HuongDan fragment = new HuongDan();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.content_main, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+        lnMeo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragMeoThi fragment = new FragMeoThi();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.content_main, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
     }
 
     @Override
